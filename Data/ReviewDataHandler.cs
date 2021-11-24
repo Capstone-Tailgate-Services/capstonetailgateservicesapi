8fcfd3fe79ecc451c044bef0d17e958599a5d51f
Joshua Wiggins <jwiggins1@crimson.ua.edu>
2021-11-23 21:50:10 -0600

    trying out new date

--- Data/ReviewDataHandler.cs
+++ Data/ReviewDataHandler.cs
@@ -16,7 +16,7 @@ namespace API.Data
         }
          public List<Review> Select(){
             db.Open();
-            string sql = "SELECT * FROM reviews";           
+            string sql = "SELECT * FROM reviews order by date desc";           
             List<ExpandoObject> results = db.Select(sql);
 
             List<Review> review = new List<Review>();
